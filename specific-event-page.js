50e42c1f8200d4bff610dfc020ae1f8e12b30d15
magnamarketing <96262983+magnamarketing@users.noreply.github.com>
2022-08-09 13:37:14 -0400

    Create specific-event-page.js

---
+++ specific-event-page.js
@@ -0,0 +1,79 @@
+let timerVar = setInterval(() => {
+    if (typeof Evergage != 'undefined') {
+        clearInterval(timerVar);
+        Evergage.init({
+
+        }).then(() => {
+            Evergage.initSitemap({
+                global: {
+                    onActionEvent: (actionEvent) => {
+                        if (/persistUserId/.test(window.location.href)) {
+                            const anonId = location.href.split("persistUserId=")[1];
+                            actionEvent.user = actionEvent.user || {};
+                            actionEvent.user.attributes = actionEvent.user.attributes || {};
+                            actionEvent.user.attributes.persistId = anonId;
+                        } 
+                        return actionEvent
+                    }
+                },
+                pageTypeDefault: {
+                    name: "TicketmasterDefault ",
+                },
+                pageTypes: [{
+                    name: "tmEventPage ",
+                    action: "Ticketmaster Event Detail",
+                    isMatch: () => true,
+                    itemAction: Evergage.ItemAction.ViewCart,
+                    catalog: {
+                        Product: {
+                            lineItems: {
+                                _id: ['$event_id$'],
+                                price: [100],
+                                quantity: [1]
+                            }
+                        }
+                    }
+                }]
+            });
+        });
+        
+    }
+}, 2000)
+
+const sendProdView = () => {
+    Evergage.sendEvent({
+        itemAction: Evergage.ItemAction.ViewItem,
+        action: "View Item",
+        catalog: {
+            Product: {
+                _id: '$event_id$',
+                name: '$event_name$',
+                categories: ['TICKETS|$artist_name$'.toUpperCase()]
+            }
+        }
+    })
+}
+
+const sendUserId = () => {
+    if (/persistUserId/.test(window.location.href)) {
+        try {
+            const anonId = location.href.split("persistUserId=")[1];
+            Evergage.sendEvent({
+                name: "Ticketmaster ID merge",
+                user: {
+                    attributes: {
+                        persistId: anonId
+                    }
+                }
+            })
+        } catch (e) {
+            Evergage.sendEvent({
+                name: "Ticketmaster ID merge Failed",
+                action: "Ticketmaster ID merge Failed"
+            })
+        }
+    }
+
+}
+sendUserId();
+sendProdView();
